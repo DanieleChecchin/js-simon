302f3cedf77b77e56f61d60fe03a6d216e82cc07
DanieleChecchin <danielechecchin26@gmail.com>
2024-08-27 18:06:47 +0200

    ESERCIZIO COMPLETATO

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ SCALETTA
 - Faccio comparire 5 input diversi vuoti dove inserirà i numeri l'utente -V-
 - Faccio comparire il bottone per confermare -V-
 - Prendo i valori degli input inseriti dall'utente -V-
-- Comparo i numeri inseriti dall'utente con quelli usciti in precedenza
+- Comparo i numeri inseriti dall'utente con quelli usciti in precedenza -V-
 - Creo un messaggio che indici quali numeri sono stati indovinati e quanti sono
 */
 
@@ -28,39 +28,41 @@ const numberField2 = document.getElementById('field2');
 const numberField3 = document.getElementById('field3');
 const numberField4 = document.getElementById('field4');
 const numberField5 = document.getElementById('field5');
+const message = document.getElementById('message');
 
 // Funzione per prendere i numeri inseriti dall'utente
 
-const userNumbers = [];
+let userNumbers = [];
+let correctNumbers = [];
 
 const userChoices = () => {
     playButton.addEventListener('click', function () {
-
         // Prendo i numeri inseriti dall'utente
         const number1 = numberField1.value;
         const number2 = numberField2.value;
         const number3 = numberField3.value;
         const number4 = numberField4.value;
         const number5 = numberField5.value;
-
         // Li inserisco in un array
         userNumbers.push(number1, number2, number3, number4, number5);
+        console.log('numeri utente ' + userNumbers);
     })
 }
 
 // Creo un array vuoto
-const numbers = [];
+let numbers = [];
 
 // Genero i 5 numeri casuali e li inserisco nell'array
 for (let i = 0; i < 5; i++) {
     let randomNumbers = Math.floor(Math.random() * 100) + 1;
     numbers.push(randomNumbers);
+    console.log('numeri pc ' + numbers);
 }
 // Stampo i numeri in Pagina
 numbersElement.innerText = numbers;
 
 // Imposto i secondi da cui devo partire per il countdown
-let time = 10;
+let time = 5;
 
 // Creo una funzione per il countdown di 30 secondi
 const countdown = setInterval(() => {
@@ -74,8 +76,18 @@ const countdown = setInterval(() => {
         inputsElement.classList.remove('none');// Faccio comparire i 5 input in cui inserire i numeri 
         playButton.classList.remove('none'); // Faccio comparire il bottone per giocare
         userChoices();
-    }
 
+        // Con un ciclo confronto i 2 array e inserisco quelli uguali in correctNumbers
+        for (let i = 0; i < 5; i++) {
+            if (numbers.includes(+ userNumbers[i])) {
+                correctNumbers.push(userNumbers[i]);
+                console.log(correctNumbers);
+
+                // Creo il messaggio finale
+                message.innerText = `Hai indovinato ${correctNumbers.length} numeri: ${correctNumbers} `
+            }
+        }
+    }
 }, 1000);
 
 
